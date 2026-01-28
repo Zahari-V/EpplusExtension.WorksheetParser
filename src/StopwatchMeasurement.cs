5011dee39d3953ba24b6add8a9fde3003f4289d5
Zahari-V <zari_a99@abv.bg>
2026-01-28 18:42:43 +0200

    Stopwatch measurement.

---
+++ src/StopwatchMeasurement.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+namespace EpplusExtension.WorksheetParser;
+
+public static class StopwatchMeasurement
+{
+    public static Stopwatch PropertySetValueStopwatch = new Stopwatch();
+}
